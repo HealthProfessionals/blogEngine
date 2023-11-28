0683784f8d6847fe4b1f7bef940b75d40bb1b67c
hugolyons <hugolyons@gmail.com>
2023-11-28 11:55:32 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,88 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+renderBlogs() {
+  const { blogs, isLoading, errors } = this.state;
 
-class App extends Component {
-  state = {
-    blogs: [],
-    isLoading: true,
-    errors: [], // Maintain a list of errors
-  };
-
-  componentDidMount() {
-    console.log('Component did mount');
-
-    fetch('http://hugolyons.pythonanywhere.com/blogs')
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          // Get the error message from the response
-          return response.text().then(errorMessage => {
-            throw new Error(`Network response was not ok. Error message: ${errorMessage}`);
-          });
-        }
-      })
-      .then(data => {
-        console.log('Data received:', data);
-        this.setState({ blogs: data, isLoading: false });
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        this.setState(prevState => ({
-          errors: [...prevState.errors, error.message], // Add the error to the list of errors
-          isLoading: false,
-        }));
-      });
-  }
-
-  renderBlogs() {
-    const { blogs, isLoading, errors } = this.state;
-
-    if (errors.length > 0) {
-      // Display all errors
-      return (
-        <div>
-          {errors.map((error, index) => (
-            <p key={index}>{error}</p>
-          ))}
-        </div>
-      );
-    }
-
-    if (isLoading) {
-      return <p>Loading blogs...</p>;
-    }
-
-    if (blogs.length === 0) {
-      return <p>No blogs found.</p>;
-    }
-
+  if (errors.length > 0) {
+    // Display all errors
     return (
-      <div className="blog-grid">
-        {blogs.map(blog => (
-          <div key={blog.id} className="blog">
-            <h2>{blog.title}</h2>
-            <p>{blog.summary}</p>
-          </div>
+      <div>
+        {errors.map((error, index) => (
+          <p key={index}>Error {index + 1}: {error}</p>
         ))}
       </div>
     );
   }
 
-  render() {
-    console.log('Render method');
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>Welcome to the Blog!</p>
-        </header>
-        <main>
-          {this.renderBlogs()}
-        </main>
-      </div>
-    );
+  if (isLoading) {
+    return <p>Loading blogs...</p>;
   }
-}
 
-export default App;
+  if (blogs.length === 0) {
+    return <p>No blogs found.</p>;
+  }
+
+  return (
+    <div className="blog-grid">
+      {blogs.map(blog => (
+        <div key={blog.id} className="blog">
+          <h2>{blog.title}</h2>
+          <p>{blog.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
